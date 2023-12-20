46306399d2174c07fe9912f45d02c851ddd2708b
MARCO ANTÔNIO V. F. FILHO <123907948+mavff@users.noreply.github.com>
2023-12-20 14:30:48 -0300

    Update Mesas_e_garcons.c

--- threads_em_c/Questao1/Mesas_e_garcons.c
+++ threads_em_c/Questao1/Mesas_e_garcons.c
@@ -10,7 +10,7 @@ typedef struct{
   int mesa;
 }Pedido;
 Pedido buff[TAM_FILA];  /* buffer size = 10; */
-int items = 0; // number of items in the buffer.
+int items = 0;
 int first = 0;
 int last = 0; 
 Pedido ped;
@@ -19,7 +19,7 @@ pthread_barrier_t barrier;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t empty = PTHREAD_COND_INITIALIZER;
 pthread_cond_t fill = PTHREAD_COND_INITIALIZER;
-void *producer(void *);
+void *producer(void *);//o codigo em si se baseia na logica de produtor e consumidor vista em sala, a unica diferenca seria a quantidade de produtores e consumidores
 void *consumer(void *);
 int main() {
     pthread_t producer_thread[NUM_MESAS];
@@ -27,7 +27,7 @@ int main() {
     int *GARCONS[NUM_GARCOM]; 
     int *MESAS[NUM_MESAS];
     srand(time(NULL)); 
-    pthread_barrier_init(&barrier, NULL, NUM_MESAS + NUM_GARCOM);
+    pthread_barrier_init(&barrier, NULL, NUM_MESAS + NUM_GARCOM);//apenas para esperar que todas as mesas e pedido inicializem juntos
     for(int i = 0; i < NUM_MESAS; i++) {
       MESAS[i] =(int *) malloc(sizeof(int)); 
       *MESAS[i] = i;
@@ -44,33 +44,28 @@ int main() {
 void put(Pedido* atual){
     pthread_mutex_lock(&mutex);
     while(items == TAM_FILA) {
-        pthread_cond_wait(&empty, &mutex);
+        pthread_cond_wait(&empty, &mutex);//atingimos o tamanho maximo de fila, logo, sem novos pedido
     }
     buff[last] = *atual;
     items++; last++;
     if(last==TAM_FILA) { last = 0; } 
-    if(items == 1) { pthread_cond_broadcast(&fill); }
+    if(items == 1) { pthread_cond_broadcast(&fill); }//poucos pedido, logo, acordar os produtores/liberar entrada de novos pedidos
     pthread_mutex_unlock(&mutex); 
 }
 void *producer(void *thread_producer) {
     Pedido ped;
     int aleatorio;
-    long int a=0;
-    pthread_barrier_wait(&barrier);
+    pthread_barrier_wait(&barrier);//apenas para esperar que todas as mesas e pedido inicializem juntos
     ped.mesa=*((int*) thread_producer);
     printf("PEDIDOS DA MESA %d INICIADOS\n", ped.mesa+1);
     while(1){
-      aleatorio=rand()%5 +1;
+      aleatorio=rand()%5 +1;//apenas para chegar pedido se vier numero 3
       if(aleatorio==3){
         ped.numero=contador;
         put(&ped);
         printf("Coloquei pedido %d na fila pela mesa %d\n",contador,ped.mesa);
         contador++;
       }
-      while(a<100000000){
-        a++;
-      }
-      a=0;
     }
     printf("Produtor terminou\n");
     pthread_exit(NULL);
@@ -78,35 +73,26 @@ void *producer(void *thread_producer) {
 Pedido get() {
     Pedido finalizado;
     pthread_mutex_lock(&mutex);
-	  while(items == 0){  pthread_cond_wait(&fill, &mutex); }
-    
+    while(items == 0){  pthread_cond_wait(&fill, &mutex); }//se itens==0 parar de consumir
     finalizado = buff[first];
     items--;  first++;
-
     if(first==TAM_FILA) {first = 0; }
     if(items == TAM_FILA - 1){ pthread_cond_signal(&empty); }
-
     pthread_mutex_unlock(&mutex);
     return finalizado;
 }
 void *consumer(void *thread_consumer) {
     Pedido v;
     int aleatorio;
-    long int a=0;
-    pthread_barrier_wait(&barrier);
+    pthread_barrier_wait(&barrier);//apenas para esperar que todas as mesas e pedido inicializem juntos
     printf("GARÇOM %d INICIOU \n", *((int*) thread_consumer));
     while(1){
-      aleatorio=rand()%2 +1;
+      aleatorio=rand()%2 +1;//apenas para chegar pedido se vier numero 1
       if(aleatorio){
         v = get();
         printf(" Mesa %d consumiu pedido %d \n", v.mesa,v.numero);
-      }
-      while(a<100000000){
-        a++;
-      }
-      a=0;
+      }   
     }
-    
     printf("Consumidor %d terminou \n", *((int*) thread_consumer));
     pthread_exit(NULL);
 }
