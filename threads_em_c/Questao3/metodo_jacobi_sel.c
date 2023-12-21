683ca44e47779e72dbfd43446ce5aad94f5d82af
vsmn <69327389+Nether190@users.noreply.github.com>
2023-12-21 14:09:43 -0300

    Adicionar timer na questão 3

--- threads_em_c/Questao3/metodo_jacobi_sel.c
+++ threads_em_c/Questao3/metodo_jacobi_sel.c
@@ -1,60 +1,73 @@
+#define _POSIX_C_SOURCE 200112L
+#define _XOPEN_SOURCE   700
+#include <pthread.h>
 #include <stdio.h>
-#include <pthread.h> 
 #include <stdlib.h>
-#define TAM_MATRIZ 3
-typedef struct{
-    int var_i;
-    float A[TAM_MATRIZ][TAM_MATRIZ];
-    float X_K[TAM_MATRIZ];
-    float B[TAM_MATRIZ];
-    float Solucao[TAM_MATRIZ];
-} SEL;//Sistema de Equacoes Lineares
-void Metodo_jacobi(SEL*);
-pthread_barrier_t barrier;//uso de barreira 
-int main(){
-    SEL *equacao = malloc(sizeof(SEL)*TAM_MATRIZ);
-    pthread_t variavel_equacao[TAM_MATRIZ];
-    pthread_barrier_init(&barrier, NULL, TAM_MATRIZ);//barreira para aguardar todas as xk variaveis ficarem prontas para utilizar para o calculo de xk+1;
-    int valores_A[TAM_MATRIZ][TAM_MATRIZ]={4, 3, -5,
-                                             -2, 3, 4,
-                                             3, -4, 3};
-    int valores_X[TAM_MATRIZ] = {0, 0, 0};
-    int valores_B[TAM_MATRIZ] = {10, 13, -6};
-    for (int i = 0; i < TAM_MATRIZ; i++) {//preencher o vetor 
-        for (int j = 0; j < TAM_MATRIZ; j++) {
-            equacao[i].A[i][j] = valores_A[i][j];
-        }
-        equacao[i].X_K[i] = valores_X[i];
-        equacao[i].B[i] = valores_B[i];
-    }
-    for(int i = 0; i < TAM_MATRIZ; i++) {//criar a thread e puxar o metodo
-        equacao[i].var_i=i;
-        pthread_create(&variavel_equacao[i],NULL,Metodo_jacobi,&equacao[i]);  
-    }
-    for (int i = 0; i < TAM_MATRIZ; ++i) {
-        pthread_join(variavel_equacao[i], NULL);
-    }
-    pthread_barrier_destroy(&barrier);
-    pthread_exit(NULL);
-}
-void Metodo_jacobi(SEL* sistema){
-    int p=15,k=0;
-    int i=sistema->var_i;//variavel i=1,2,3,4...n que estamos trabalhando
-    float* x_k_mais_1=&sistema->Solucao[i];
-    float b_i=sistema->B[i];//termo Bi da matriz;
-    float a_i_i=sistema->A[i][i];//termo Aii da matriz
-    float somatorio=0;
-    while(k<p){
-        for(int j=0;j<TAM_MATRIZ;j++){//fazer o somatorio
-            if(j!=i){//i!=j
-                somatorio+=(sistema->A[i][j])*(sistema->X_K[j]);
-            }
-        }
-        *x_k_mais_1=(b_i-somatorio)/a_i_i;
-        k++;
-        somatorio=0;
-        pthread_barrier_wait(&barrier);//esperar a cada interacao para os xk de cada thread ficar pronto
+#include <time.h>
 
-    }
-    printf("Solucao da variavel X_%d : %.6f\n",i+1,*x_k_mais_1);
+
+#define TAM_MATRIZ 3
+
+typedef struct {
+	int var_i;
+	float A[TAM_MATRIZ][TAM_MATRIZ];
+	float X_K[TAM_MATRIZ];
+	float B[TAM_MATRIZ];
+	float Solucao[TAM_MATRIZ];
+} SEL;  //Sistema de Equacoes Lineares
+
+void* Metodo_jacobi(void* sistema_pt);
+
+pthread_barrier_t barrier;  //uso de barreira
+
+int main() {
+	SEL* equacao = malloc(sizeof(SEL) * TAM_MATRIZ);
+	pthread_t variavel_equacao[TAM_MATRIZ];
+	pthread_barrier_init(
+	  &barrier,
+	  NULL,
+	  TAM_MATRIZ);  //barreira para aguardar todas as xk variaveis ficarem prontas para utilizar para o calculo de xk+1;
+	int valores_A[TAM_MATRIZ][TAM_MATRIZ] = {{4, 3, -5}, {-2, 3, 4}, {3, -4, 3}};
+	int valores_X[TAM_MATRIZ] = {0, 0, 0};
+	int valores_B[TAM_MATRIZ] = {10, 13, -6};
+	for (int i = 0; i < TAM_MATRIZ; i++) {  //preencher o vetor
+		for (int j = 0; j < TAM_MATRIZ; j++) { equacao[i].A[i][j] = valores_A[i][j]; }
+		equacao[i].X_K[i] = valores_X[i];
+		equacao[i].B[i] = valores_B[i];
+	}
+
+	clock_t timer_start = clock();
+	for (int i = 0; i < TAM_MATRIZ; i++) {  //criar a thread e puxar o metodo
+		equacao[i].var_i = i;
+		pthread_create(&variavel_equacao[i], NULL, Metodo_jacobi, (void*) &equacao[i]);
+	}
+	for (int i = 0; i < TAM_MATRIZ; ++i) { pthread_join(variavel_equacao[i], NULL); }
+	clock_t timer_end = clock();
+	double timer_total = ((double) (timer_end - timer_start) * 1000.0) / CLOCKS_PER_SEC;
+	printf("Tempo gasto: %.3lf\n", timer_total);
+
+	pthread_barrier_destroy(&barrier);
+	pthread_exit(NULL);
+}
+void* Metodo_jacobi(void* sistema_pt) {
+	SEL* sistema = (SEL*) sistema_pt;
+	int p = 15, k = 0;
+	int i = sistema->var_i;  //variavel i=1,2,3,4...n que estamos trabalhando
+	float* x_k_mais_1 = &sistema->Solucao[i];
+	float b_i = sistema->B[i];  //termo Bi da matriz;
+	float a_i_i = sistema->A[i][i];  //termo Aii da matriz
+	float somatorio = 0;
+	while (k < p) {
+		for (int j = 0; j < TAM_MATRIZ; j++) {  //fazer o somatorio
+			if (j != i) {  //i!=j
+				somatorio += (sistema->A[i][j]) * (sistema->X_K[j]);
+			}
+		}
+		*x_k_mais_1 = (b_i - somatorio) / a_i_i;
+		k++;
+		somatorio = 0;
+		pthread_barrier_wait(&barrier);  //esperar a cada interacao para os xk de cada thread ficar pronto
+	}
+	printf("Solucao da variavel X_%d : %.6f\n", i + 1, *x_k_mais_1);
+	return NULL;
 }
